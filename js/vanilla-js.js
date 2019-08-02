460d3d17548417cd4c2c583f892eb7c13fb1db34
Sami Nooor <32134109+Samileono@users.noreply.github.com>
2019-08-02 23:29:16 +1400

    Update vanilla-js.js

--- js/vanilla-js.js
+++ js/vanilla-js.js
@@ -1,17 +1,14 @@
 const sideNav = document.querySelector("#sidenav");
 const toggleBtn = document.querySelector("span.toggle-btn");
 const closeBtn = document.querySelector(".closeBtn");
-const main  = document.querySelector("#main");
 
 toggleBtn.addEventListener("click", openSideNav);
 closeBtn.addEventListener("click", closeSideNav);
 
 function openSideNav() {
     sideNav.style = `left: 0px; transition: .2s ease-in-out; width: 242px; `;
-    main.style = `transition: all .21s linear; margin-left: 242px; opacity: .2`;
 };
 
 function closeSideNav() {
     sideNav.style = `left: -1000px; transition: .2s ease-in-out; width: 0`;
-    main.style = `transition: all .21s linear; margin-left: 0px;`;
 }
